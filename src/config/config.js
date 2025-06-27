4a9abf02666640a804199998c8b451180fccfc6a
bhargav.harvani <bhargav.harvani@esparkbizmail.com>
2025-06-27 18:28:26 +0530

    big-smal change

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 module.exports = {
   development: {
     username: "postgres",
@@ -13,11 +14,21 @@ module.exports = {
     host: "127.0.0.1",
     dialect: "mysql",
   },
+  // production: {
+  //   username: "root",
+  //   password: null,
+  //   database: "database_production",
+  //   host: "127.0.0.1",
+  //   dialect: "mysql",
+  // },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // important for Railway
+      },
+    },
   },
 };
